fbae378d6f68d17bf3e0209f5137f27f915c2f63
adrianadermon <adrian.adermon@gmail.com>
2025-01-14 08:15:59 +0100

    Updated colors and linetypes

--- R/scales.R
+++ R/scales.R
@@ -5,10 +5,10 @@ colors_ifau <- function(n) {
   colors <- c("#12304A", # IFAU blue
               "#F2C913",  # IFAU yellow
               "#5F7562", # gray-green
-              "#BA5F6D", # pink
+              "#CF6977", # pink
               "#61445A", # dark purple
-              "#5C7987", # gray-blue
-              "#CFD3D4") # IFAU gray
+              "#446775", # gray-blue
+              "#B6B9BA") # IFAU gray
   colors[1:n]
 }
 
@@ -21,13 +21,13 @@ colors_ifau_fun <- function() {
 #' @param n Number of shapes
 shapes_ifau <- function(n) {
   shapes <- c(
-    16,
-    15,
-    17,
-    18,
-    3,
-    4,
-    1)
+    16, # ●
+    15, # ■
+    17, # ▲
+    18, # ◆
+    3,  # +
+    4,  # ×
+    1)  # ◯
   shapes[1:n]
 }
 
@@ -40,12 +40,12 @@ shapes_ifau_fun <- function() {
 #' @param n Number of linetypes
 linetypes_ifau <- function(n) {
   linetypes <- c("solid",
-                 "51111111", # long dash, three dots
-                 "32", # dash 
+                 "32", # short dash 
+                 "11", # dot
+                 "3212", # dash-dot
+                 "63", # long dash
                  "211111", # short dash, two dots
-                 "52", # long dash
-                 "11", # tight dots
-                 "21") # short dash
+                 "62121212") # long dash, three dots
   linetypes[1:n]
 }
 
