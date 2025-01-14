6481a14e3e41e20fd242972524337c6f5d9e5e7c
adrianadermon <adrian.adermon@gmail.com>
2025-01-14 08:18:32 +0100

    Update geoms

--- R/update_geoms.R
+++ R/update_geoms.R
@@ -1,6 +1,7 @@
 ## Save original defaults
 line_defaults <- ggplot2::GeomLine$default_aes
 vline_defaults <- ggplot2::GeomVline$default_aes
+hline_defaults <- ggplot2::GeomHline$default_aes
 bar_defaults <- ggplot2::GeomBar$default_aes
 
 #' Update geoms
@@ -9,11 +10,16 @@ bar_defaults <- ggplot2::GeomBar$default_aes
 #'
 #' @export
 set_geoms_ifau <- function() {
-        ggplot2::update_geom_defaults("line", ggplot2::aes(linewidth = 0.5))
+        ggplot2::update_geom_defaults("line", ggplot2::aes(linewidth = 1/(0.75*.pt)))
         ggplot2::update_geom_defaults("vline", ggplot2::aes(
-                linewidth = 0.25 / 0.75,
-                color = "#A8ACAD",
-                linetype = "42"
+                linewidth = 0.5/(0.75*.pt),
+                color = "#8B8E8F",
+                linetype = "solid"
+        ))
+        ggplot2::update_geom_defaults("hline", ggplot2::aes(
+                linewidth = 0.5/(0.75*.pt),
+                color = "#8B8E8F",
+                linetype = "solid"
         ))
         ggplot2::update_geom_defaults("bar", ggplot2::aes(fill = colors_ifau(1)))
 }
@@ -26,5 +32,6 @@ set_geoms_ifau <- function() {
 unset_geoms_ifau <- function() {
   ggplot2::update_geom_defaults("line", line_defaults)
   ggplot2::update_geom_defaults("vline", vline_defaults)
+  ggplot2::update_geom_defaults("hline", hline_defaults)
   ggplot2::update_geom_defaults("bar", bar_defaults)
 }
