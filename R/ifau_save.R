4f7d13d0cbfc4ecfb24c46bd6da77c2ff13aa04f
Adrian Adermon <adrian.adermon@gmail.com>
2025-01-17 12:18:16 +0100

    Updated default height of saved figures

--- R/ifau_save.R
+++ R/ifau_save.R
@@ -10,5 +10,5 @@ ifau_save <- ggplot2::ggsave
 
 ## Update defaults
 formals(ifau_save)$width = 12.4
-formals(ifau_save)$height = 6.9
+formals(ifau_save)$height = 7.4
 formals(ifau_save)$units = "cm"
